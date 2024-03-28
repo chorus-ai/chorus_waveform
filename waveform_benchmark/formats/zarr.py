57ec685388b7208faa6c65bd2c5693c5296732de
del42 <delger42@gmail.com>
2024-03-28 13:53:18 -0400

    Zarr format handler added

---
+++ waveform_benchmark/formats/zarr.py
@@ -0,0 +1,46 @@
+import numpy as np
+import zarr
+from waveform_benchmark.formats.base import BaseFormat
+
+
+class Zarr(BaseFormat):
+    """
+    Example format using Zarr 
+    """
+
+    def write_waveforms(self, path, waveforms):
+        # Initialize Zarr group 
+        root_group = zarr.open_group(path, mode='w')
+
+        for name, waveform in waveforms.items():
+            length = waveform['chunks'][-1]['end_sample']
+            samples = np.empty(length, dtype=np.float32)
+            samples[:] = np.nan 
+
+            for chunk in waveform['chunks']:
+                start = chunk['start_sample']
+                end = chunk['end_sample']
+                samples[start:end] = chunk['samples']
+
+            # Create a dataset for each waveform within the root group.
+            ds = root_group.create_dataset(name, data=samples, chunks=True, dtype=np.float32)
+            ds.attrs['units'] = waveform['units']
+            ds.attrs['samples_per_second'] = waveform['samples_per_second']
+
+
+    def read_waveforms(self, path, start_time, end_time, signal_names):
+        # Open the Zarr group
+        root_group = zarr.open_group(path, mode='r')
+
+        results = {}
+        for signal_name in signal_names:
+            ds = root_group[signal_name]
+            samples_per_second = ds.attrs['samples_per_second']
+            
+            start_sample = round(start_time * samples_per_second)
+            end_sample = round(end_time * samples_per_second)
+            
+            # Random access the Zarr array 
+            results[signal_name] = ds[start_sample:end_sample]
+
+        return results
