95e3af000a2f7a1b7995e806bdd88ee21ff48239
Ashe <WA6GZ@uvahealth.org>
2024-04-01 15:28:22 -0400

    Adds fidelity check

--- waveform_benchmark/__main__.py
+++ waveform_benchmark/__main__.py
@@ -1,16 +1,22 @@
 import argparse
 
 from waveform_benchmark.benchmark import run_benchmarks
-
+from waveform_benchmark.formattest import run_conversion
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('input_record')
     ap.add_argument('format_class')
+    ap.add_argument('-c', '--check',
+                    action='store_true')
     opts = ap.parse_args()
 
-    run_benchmarks(input_record=opts.input_record,
+    if(opts.check):
+        run_conversion(input_record=opts.input_record,
                    format_class=opts.format_class)
+    else:
+        run_benchmarks(input_record=opts.input_record,
+                   format_class=opts.format_class)          
 
 
 if __name__ == '__main__':
