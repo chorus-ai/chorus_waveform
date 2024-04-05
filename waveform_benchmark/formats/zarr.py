5793fbc944c2d70b3860e24ee64ea05eb8abfcbc
del42 <delger42@gmail.com>
2024-04-05 10:53:24 -0400

    zarr format compressed signal handler added

--- waveform_benchmark/formats/zarr.py
+++ waveform_benchmark/formats/zarr.py
@@ -44,3 +44,45 @@ class Zarr(BaseFormat):
             results[signal_name] = ds[start_sample:end_sample]
 
         return results
+
+class Zarr_compressed(BaseFormat):
+    """
+    Example format using Zarr with compression.
+    """
+
+    def write_waveforms(self, path, waveforms):
+        # Initialize Zarr group 
+        root_group = zarr.open_group(path, mode='w')
+
+        for name, waveform in waveforms.items():
+            length = waveform['chunks'][-1]['end_sample']
+            samples = np.empty(length, dtype=np.float32)
+            samples[:] = np.nan 
+
+            for chunk in waveform['chunks']:
+                start = chunk['start_sample']
+                end = chunk['end_sample']
+                samples[start:end] = chunk['samples']
+
+            # each waveform within the root group with compression.
+            ds = root_group.create_dataset(name, data=samples, chunks=True, dtype=np.float32, compressor=zarr.Blosc(cname='zstd', clevel=9, shuffle=zarr.Blosc.BITSHUFFLE))
+            ds.attrs['units'] = waveform['units']
+            ds.attrs['samples_per_second'] = waveform['samples_per_second']
+
+
+    def read_waveforms(self, path, start_time, end_time, signal_names):
+        # Open the Zarr group
+        root_group = zarr.open_group(path, mode='r')
+
+        results = {}
+        for signal_name in signal_names:
+            ds = root_group[signal_name]
+            samples_per_second = ds.attrs['samples_per_second']
+            
+            start_sample = round(start_time * samples_per_second)
+            end_sample = round(end_time * samples_per_second)
+            
+            # Random access the Zarr array 
+            results[signal_name] = ds[start_sample:end_sample]
+
+        return results
