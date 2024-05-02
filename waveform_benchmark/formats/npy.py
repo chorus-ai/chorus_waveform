668aefea57a2bafd581e8649d4483a853329c396
del42 <delger42@gmail.com>
2024-05-02 16:23:48 -0400

    Updated npy format handler

--- waveform_benchmark/formats/npy.py
+++ waveform_benchmark/formats/npy.py
@@ -9,36 +9,27 @@ class NPY(BaseFormat):
 
     def write_waveforms(self, path, waveforms):
         # Convert each channel into an array with no gaps.
-        flattened_waveforms = {}
         for name, waveform in waveforms.items():
             length = waveform['chunks'][-1]['end_sample']
-            samples = numpy.empty(length, dtype=numpy.float32)
-            samples[:] = numpy.nan
+            samples = numpy.full(length, numpy.nan, dtype=numpy.float32)
+
             for chunk in waveform['chunks']:
                 start = chunk['start_sample']
                 end = chunk['end_sample']
                 samples[start:end] = chunk['samples']
 
-            flattened_waveforms[name] = {
-                'units': waveform['units'],
-                'samples_per_second': waveform['samples_per_second'],
-                'samples': samples,
-            }
-
-        for name, data in flattened_waveforms.items():
-            waveform_path = f"{path}_{name}.npy"
-            numpy.save(waveform_path, data)
+            # Save the samples as an NPY file. 
+            numpy.save(f"{path}_{name}.npy", (samples, waveform['samples_per_second']))
 
     def read_waveforms(self, path, start_time, end_time, signal_names):
-        # Read arrays from NPY files.
         results = {}
         for signal_name in signal_names:
             waveform_path = f"{path}_{signal_name}.npy"
-            data = numpy.load(waveform_path, allow_pickle=True).item()
+            data, fs = numpy.load(waveform_path, allow_pickle=True)
 
-            start_sample = round(start_time / data['samples_per_second'])
-            end_sample = round(end_time / data['samples_per_second'])
-            samples = data['samples'][start_sample:end_sample]
+            start_sample = round(start_time * fs)
+            end_sample = round(end_time * fs)
+            samples = data[start_sample:end_sample]
 
             results[signal_name] = samples
         return results
