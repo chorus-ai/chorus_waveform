1abbec65399f31ee92551784cc40410b09ea21cd
del42 <delger42@gmail.com>
2024-05-03 14:25:45 -0400

    Updated npy handler to work on python3.11

--- waveform_benchmark/formats/npy.py
+++ waveform_benchmark/formats/npy.py
@@ -1,35 +1,46 @@
 import numpy
-
 from waveform_benchmark.formats.base import BaseFormat
 
 class NPY(BaseFormat):
     """
-    Example format using NPY for storage.
+    Example format using NPY.
     """
 
     def write_waveforms(self, path, waveforms):
         # Convert each channel into an array with no gaps.
         for name, waveform in waveforms.items():
             length = waveform['chunks'][-1]['end_sample']
-            samples = numpy.full(length, numpy.nan, dtype=numpy.float32)
+            samples = numpy.full(length + 1, numpy.nan, dtype=numpy.float32)
+
+            # Store the sampling rate as the first element
+            samples[0] = waveform['samples_per_second']
 
             for chunk in waveform['chunks']:
                 start = chunk['start_sample']
                 end = chunk['end_sample']
-                samples[start:end] = chunk['samples']
 
-            # Save the samples as an NPY file. 
-            numpy.save(f"{path}_{name}.npy", (samples, waveform['samples_per_second']))
+                # Shift the indices by 1 to accommodate the sampling rate
+                samples[start + 1:end + 1] = chunk['samples']
+
+            # Save the samples as an NPY file.
+            numpy.save(f"{path}_{name}.npy", samples)
 
     def read_waveforms(self, path, start_time, end_time, signal_names):
         results = {}
         for signal_name in signal_names:
             waveform_path = f"{path}_{signal_name}.npy"
-            data, fs = numpy.load(waveform_path, allow_pickle=True)
+            samples = numpy.load(waveform_path)
+
+            # Extract the sampling rate from the first element
+            fs = samples[0]
+            
+            # exclude the sampling rate from the samples
+            samples = samples[1:]
 
             start_sample = round(start_time * fs)
             end_sample = round(end_time * fs)
-            samples = data[start_sample:end_sample]
+            samples = samples[start_sample:end_sample]
 
             results[signal_name] = samples
+
         return results
