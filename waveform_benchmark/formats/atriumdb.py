cfde63095f76260fa6f448882cc8941a872ba24f
William Dixon <william.dixon@torontomu.ca>
2024-10-08 10:28:02 -0400

    Change query style to standard, add Nan adapter class, add buffered writing.

--- waveform_benchmark/formats/atriumdb.py
+++ waveform_benchmark/formats/atriumdb.py
@@ -20,90 +20,36 @@ class AtriumDB(BaseFormat):
         if sdk is None:
             sdk = AtriumSDK.create_dataset(dataset_location=path)
             sdk = AtriumSDK(dataset_location=path, num_threads=1)
-            # Set the block size to 16 times less than the default.
-            sdk.block.block_size = 131072 // 8
+            sdk.block.block_size = 16384
+
         device_tag = "chorus"
         chorus_device_id = sdk.insert_device(device_tag=device_tag)
         sdk.get_device_info(chorus_device_id)
 
         # Convert each channel into an array with no gaps.
         for name, waveform in waveforms.items():
-            freq_hz = waveform['samples_per_second']
-            freq_nhz = round(freq_hz * (10 ** 9))
-            period_ns = (10 ** 18) // freq_nhz
-            measure_id = sdk.insert_measure(measure_tag=name, freq=freq_hz, freq_units="Hz")
-
-            # Convert chunks into an array with no gaps.
-            sig_gain = 0
-            waveform_start = None
-            time_chunks, value_chunks = [], []
-            for chunk in waveform['chunks']:
-                value_data = chunk['samples']
-                start_time_nano = int(np.round(chunk['start_time'] * float(10 ** 9)))
-                waveform_start = start_time_nano if waveform_start is None else min(start_time_nano, waveform_start)
-
-                time_data = np.arange(value_data.size, dtype=np.int64) * period_ns + start_time_nano
-                time_chunks.append(time_data)
-                value_chunks.append(value_data)
-                sig_gain = max(sig_gain, chunk['gain'])
-
-            if len(time_chunks) == 0:
+            if len(waveform['chunks']) == 0:
                 continue
-            time_data = np.concatenate(time_chunks, dtype=np.int64)
-            value_data = np.concatenate(value_chunks, dtype=value_chunks[0].dtype)
+            with sdk.write_buffer() as buf:
+                freq_hz = waveform['samples_per_second']
+                measure_id = sdk.insert_measure(measure_tag=name, freq=freq_hz, freq_units="Hz")
 
-            sig_baseline = 0
+                # Calculate digital to analog scale factors.
+                sig_gain = max(chunk['gain'] for chunk in waveform['chunks'])
+                sig_baseline = 0
 
-            # Remove NaN values from value_data and the corresponding indices from time_data
-            non_nan_indices = ~np.isnan(value_data)
-            value_data = value_data[non_nan_indices]
-            time_data = time_data[non_nan_indices]
-
-            # Check if all digital values are integers
-            digital_values = (value_data * sig_gain) - sig_baseline
-            # digital_values_are_all_ints = np.all(np.isclose(digital_values, np.round(digital_values)))
-            digital_values_are_all_ints = True
-
-            scale_m, scale_b = None, None
-            if digital_values_are_all_ints:
-                value_data = np.round(digital_values).astype(np.int64)
                 scale_m = 1 / sig_gain
                 scale_b = float(sig_baseline) / sig_gain
 
-            if time_data.size == 0:
-                continue
-
-            # Determine the raw and encoded value types based on the dtype of value_data
-            if np.issubdtype(value_data.dtype, np.integer):
-                raw_v_t = 1
-                encoded_v_t = 3
-            else:
-                raw_v_t = 2
-                encoded_v_t = 2
-
-            _, _, _, filename = sdk.write_data(
-                measure_id, chorus_device_id, time_data, value_data, freq_nhz, int(time_data[0]),
-                raw_time_type=1, raw_value_type=raw_v_t, encoded_time_type=2, encoded_value_type=encoded_v_t,
-                scale_m=scale_m, scale_b=scale_b)
-
-            block_list, block_start_list, block_end_list = get_block_data(sdk, measure_id, chorus_device_id)
-            if len(block_list) == 0:
-                raise ValueError("Cannot save header information, no blocks were written")
-            file_id = block_list[0][3]
-
-            # Save header information to file
-            meta_dir = Path(path) / "meta"
-            meta_dir.mkdir(parents=True, exist_ok=True)
-            header_filename = meta_dir / f"{measure_id}_{chorus_device_id}.pkl"
-            header_data = {
-                "block_list": block_list,
-                "block_start_list": block_start_list,
-                "block_end_list": block_end_list,
-                "file_id": file_id,
-                "filename": filename
-            }
-            with open(header_filename, 'wb') as f:
-                pickle.dump(header_data, f)
+                for chunk in waveform['chunks']:
+                    value_data = chunk['samples']
+                    start_time_s = chunk['start_time']
+                    for segment_start_s, segment_values in generate_non_nan_slices(start_time_s, freq_hz, value_data):
+                        # convert analog data to digital for optimal storage
+                        digital_values = (segment_values * sig_gain) - sig_baseline
+                        digital_values = np.round(digital_values).astype(np.int64)
+                        sdk.write_segment(measure_id, chorus_device_id, digital_values, segment_start_s, freq=freq_hz,
+                                          freq_units="Hz", time_units="s", scale_m=scale_m, scale_b=scale_b)
 
     def read_waveforms(self, path, start_time, end_time, signal_names):
         assert sdk is not None, "SDK should have been initialized in writing phase"
@@ -114,12 +60,18 @@ class AtriumDB(BaseFormat):
         measures = {measure['tag']: (measure['id'], measure['freq_nhz']) for _, measure in sdk._measures.items()}
         new_device_id = sdk.get_device_id("chorus")
 
+        # If the block metadata hasn't been read, read them.
+        if len(sdk.block_cache) == 0:
+            sdk.load_device(new_device_id)
+
         # Read Data
         results = {}
         for signal_name in signal_names:
             new_measure_id, freq_nhz = measures[signal_name]
-            header_data = read_header_data(path, new_measure_id, new_device_id)
-            if header_data is None:
+
+            _, read_time_data, read_value_data = sdk.get_data(new_measure_id, start_time_nano, end_time_nano, device_id=new_device_id, sort=False)
+
+            if read_value_data.size == 0:
                 freq_hz = freq_nhz / (10 ** 9)
                 start_frame = round(start_time * freq_hz)
                 end_frame = round(end_time * freq_hz)
@@ -129,33 +81,6 @@ class AtriumDB(BaseFormat):
                     nan_values[:] = np.nan
                 results[signal_name] = nan_values
                 continue
-
-            filename_dict = {header_data["file_id"]: header_data["filename"]}
-
-            # Get blocks from cache
-            block_list = find_blocks(header_data["block_list"], header_data["block_start_list"],
-                                     header_data["block_end_list"], start_time_nano, end_time_nano)
-
-            if len(block_list) == 0:
-                freq_hz = freq_nhz / (10 ** 9)
-                start_frame = round(start_time * freq_hz)
-                end_frame = round(end_time * freq_hz)
-                num_samples = end_frame - start_frame
-                nan_values = np.empty(num_samples, dtype=np.float32)
-                if num_samples > 0:
-                    nan_values[:] = np.nan
-                results[signal_name] = nan_values
-                continue
-
-            read_list = condense_byte_read_list(block_list)
-            encoded_bytes = sdk.file_api.read_file_list(read_list, filename_dict)
-
-            # Extract the number of bytes for each block
-            num_bytes_list = [row[5] for row in block_list]
-
-            # Decode the data and separate it into headers, times, and values
-            read_time_data, read_value_data, headers = sdk.block.decode_blocks(encoded_bytes, num_bytes_list, analog=True,
-                                                                  time_type=1)
 
             # Truncate unneeded values.
             left = np.searchsorted(read_time_data, start_time_nano, side='left')
@@ -167,67 +92,64 @@ class AtriumDB(BaseFormat):
         return results
 
 
-def get_block_data(block_sdk, measure_id, device_id):
-    query = """
-    SELECT id, measure_id, device_id, file_id, start_byte, num_bytes, start_time_n, end_time_n, num_values
-    FROM block_index
-    WHERE measure_id = ? AND device_id = ?
-    ORDER BY measure_id, device_id, start_time_n ASC;
+class NanAdaptedAtriumDB(AtriumDB):
+    """
+    AtriumDBWithNan: Subclass of AtriumDB to convert time, value tuples into a single NaN array.
     """
 
-    with block_sdk.sql_handler.connection() as (conn, cursor):
-        cursor.execute(query, (measure_id, device_id))
-        block_query_result = cursor.fetchall()
+    def read_waveforms(self, path, start_time, end_time, signal_names):
+        assert sdk is not None, "SDK should have been initialized in writing phase"
 
-    block_start_list, block_end_list = [], []
-    for block in block_query_result:
-        block_id, measure_id, device_id, file_id, start_byte, num_bytes, start_time, end_time, num_values = block
-        block_start_list.append(start_time)
-        block_end_list.append(end_time)
+        start_time_nano = int(start_time * (10 ** 9))
+        end_time_nano = int(end_time * (10 ** 9))
 
-    return block_query_result, block_start_list, block_end_list
+        measures = {measure['tag']: (measure['id'], measure['freq_nhz']) for _, measure in sdk._measures.items()}
+        new_device_id = sdk.get_device_id("chorus")
 
+        # If the block metadata hasn't been read, read them.
+        if len(sdk.block_cache) == 0:
+            sdk.load_device(new_device_id)
 
-def read_header_data(path, measure_id, device_id):
-    meta_dir = Path(path) / "meta"
-    header_filename = meta_dir / f"{measure_id}_{device_id}.pkl"
+        # Read Data
+        results = {}
+        for signal_name in signal_names:
+            new_measure_id, freq_nhz = measures[signal_name]
 
-    if not header_filename.exists():
-        return None
+            _, read_value_data = sdk.get_data(
+                new_measure_id, start_time_nano, end_time_nano, device_id=new_device_id, return_nan_gap=True)
 
-    with open(header_filename, 'rb') as f:
-        header_data = pickle.load(f)
+            results[signal_name] = read_value_data
 
-    return header_data
+        return results
 
+def generate_non_nan_slices(start_time_s: float, freq_hz: float, data: np.ndarray):
+    dt = 1.0 / freq_hz
+    non_nan_mask = ~np.isnan(data)
 
-def find_blocks(blocks, starts, ends, start_time, end_time):
-    start_idx = bisect.bisect_left(ends, start_time)
-    end_idx = bisect.bisect_left(ends, end_time)
+    if len(non_nan_mask) == 0:
+        return
 
-    if start_idx == end_idx:
-        if start_idx >= len(starts):
-            return []
-        if (not (starts[start_idx] <= start_time <= ends[start_idx])
-                and not (starts[end_idx] <= end_time <= ends[end_idx])):
-            return []
+    # Find the start indices of continuous non-NaN slices
+    start_indices = []
+    if non_nan_mask[0]:
+        start_indices.append(0)
+    start_indices.extend(
+        (np.where((~non_nan_mask[:-1]) & (non_nan_mask[1:]))[0] + 1).tolist()
+    )
 
-    if end_idx < len(starts) and end_time < starts[end_idx]:
-        end_idx = max(0, end_idx - 1)
+    # Find the end indices of continuous non-NaN slices
+    end_indices = (
+            np.where((non_nan_mask[:-1]) & (~non_nan_mask[1:]))[0] + 1
+    ).tolist()
+    if non_nan_mask[-1]:
+        end_indices.append(len(non_nan_mask))
 
-    return blocks[start_idx:end_idx + 1]
+    # Convert lists to numpy arrays for efficient indexing
+    start_indices = np.array(start_indices)
+    end_indices = np.array(end_indices)
 
-
-def condense_byte_read_list(block_list):
-    result = []
-
-    for row in block_list:
-        if len(result) == 0 or result[-1][2] != row[3] or result[-1][3] + result[-1][4] != row[4]:
-            # append measure_id, device_id, file_id, start_byte and num_bytes
-            result.append([row[1], row[2], row[3], row[4], row[5]])
-        else:
-            # if the blocks are continuous merge the reads together by adding the size of the next block to the
-            # num_bytes field
-            result[-1][4] += row[5]
-
-    return result
+    # Yield the slices along with their corresponding start times
+    for i0, i1 in zip(start_indices, end_indices):
+        t_i0 = start_time_s + i0 * dt
+        data_slice = data[i0:i1]
+        yield t_i0, data_slice
